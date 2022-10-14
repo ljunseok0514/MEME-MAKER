418898403c75f225ae3f4eb37c84f58eccf0369f
ljunseok0514 <73566234+ljunseok0514@users.noreply.github.com>
2022-10-14 17:51:58 +0900

    221014

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.fill();
+ctx.lineWidth = 2;
+
+const colors = [
+  "#f6b93b",
+  "#e55039",
+  "#4a69bd",
+  "#60a3bc",
+  "#78e08f",
+  "#fa983a",
+  "#eb2f06",
+  "#1e3799",
+  "#3c6382",
+  "#38ada9",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
